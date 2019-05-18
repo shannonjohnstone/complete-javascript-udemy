d5d0c3dcb2a147836718bfc6b5f5c2c679049436
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-05-19 03:21:58 +1000

    add script content, basic varibles, logs and undefined example

--- 002-language-basics/script.js
+++ 002-language-basics/script.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+/*eslint no-console: 0*/
+
+const firstName = 'Dustin';
+console.log(firstName);
+
+const lastName = 'Dog';
+const age = 6;
+
+const fullAge = true;
+console.log(fullAge);
+
+let job;;
+console.log(job);
