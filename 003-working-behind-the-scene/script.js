72eed503fa03cae55477c445072d4370c6b9ea41
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-05-19 21:46:36 +1000

    this context

--- 003-working-behind-the-scene/script.js
+++ 003-working-behind-the-scene/script.js
@@ -48,3 +48,50 @@ function third() {
   //   console.log(a + b + c + d, 'log from third()');
   console.log(a + d, 'log from third()');
 }
+
+/*************************************************
+ * this keyword
+ */
+sectionLog('this keyword');
+// console.log(this);
+function calculateAgeWithThis(year) {
+  console.log(2019 - year);
+  console.log(this, calculateAgeWithThis);
+}
+calculateAgeWithThis(1984);
+
+const dustin = {
+  name: 'Dustin',
+  yearOfBirth: 1990,
+  calculateAge: function() {
+    console.log(2019 - this.yearOfBirth);
+    // this refers to the dustin object as it was the object that executed the caluculateAge method
+    console.log(this, 'dustin');
+
+    /**
+     * this in innerFunction will endup referencing the `Window` object
+     * this is because the rule says `a regualer function will reference the global context
+     * and this is a regular function that is not call from a object
+     */
+    // function innerFunction() {
+    //   console.log(this, 'innerFunction');
+    // }
+    // innerFunction();
+  },
+};
+
+dustin.calculateAge();
+
+const mike = {
+  name: 'Mike',
+  yearOfBirth: 1980,
+};
+
+/**
+ * The fact that the this keyword only gets assigned a value when its executed
+ * means we can use the same calculateAge metohd from the dustin object even tho
+ * its already called
+ */
+mike.calculateAge = dustin.calculateAge;
+
+mike.calculateAge();
