5ddf74173299fe7668ac79cac1a5b9c334a32eee
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-05-26 16:00:10 +1000

    primitive vs objects

--- 005-objects-functions/script.js
+++ 005-objects-functions/script.js
@@ -22,3 +22,34 @@ const zag = new Person('Zag', 2019, null);
 const dustin = new Person('Dustin', 2016, 'Deer Hunter');
 console.log({ zag, age: zag.calculateAge() });
 console.log({ dustin, age: dustin.calculateAge() });
+
+sectionLog('Object.create');
+
+const personProto = {
+  calculateAge: function() {
+    const year = new Date().getFullYear();
+    return year - this.yearOfBirth;
+  },
+};
+
+const tom = Object.create(personProto, {
+  name: { value: 'Tom' },
+  yearOfBirth: { value: 1990 },
+  job: { value: 'Bus Driver' },
+});
+
+console.log({ tom });
+
+sectionLog('Primitive vs Object');
+
+let a = 23;
+let b = a;
+a = 10;
+
+console.log({ a, b });
+
+const obj1 = { name: 'Jack', age: 20 };
+const obj2 = obj1;
+obj1.age = 40;
+
+console.log({ obj1, obj2 });
