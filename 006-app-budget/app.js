5fc23e30ada83fbb64f1d9f5881538feba3e15e1
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-05-28 11:26:34 +1000

    update UI ctrl and main controller

--- 006-app-budget/app.js
+++ 006-app-budget/app.js
@@ -1,3 +1,8 @@
+const enterPressed = e => e.keyCode === 13;
+const when = (cond, f) => x => (cond(x) ? f(x) : x);
+const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+const getElement = v => document.querySelector(v);
+
 const budgetController = (() => {
   const x = 10;
   const add = a => x + a;
@@ -11,15 +16,42 @@ const budgetController = (() => {
 })();
 
 const UIController = (() => {
-  // some code
+  const DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
+
+  return {
+    getInput: () => {
+      const type = getElement(DOMStrings.inputType).value;
+      const description = getElement(DOMStrings.inputDescription).value;
+      const value = getElement(DOMStrings.inputValue).value;
+      return { type, description, value };
+    },
+    getDOMStrings: () => DOMStrings,
+  };
 })();
 
 const controller = ((budgetCtrl, UICtrl) => {
-  const val = budgetCtrl.publicTest(8);
+  const DOM = UICtrl.getDOMStrings();
 
-  return {
-    publicCalc: function() {
-      return val;
-    },
+  const contolAddItem = function() {
+    const input = UICtrl.getInput();
+    console.log(input);
+
+    /**
+     * - get field input data
+     * - add new item to budget controller
+     * - add item to UI
+     * - calculate budget
+     * - display the budget on UI
+     */
   };
+
+  const controlOnEnter = compose(when(enterPressed, contolAddItem));
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', contolAddItem);
+  document.addEventListener('keypress', controlOnEnter);
 })(budgetController, UIController);
