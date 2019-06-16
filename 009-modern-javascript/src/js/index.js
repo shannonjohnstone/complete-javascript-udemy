93b525824f963fdf8074450d6c22e0455c4ba04f
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-06-16 14:16:06 +1000

    basic api request method

--- 009-modern-javascript/src/js/index.js
+++ 009-modern-javascript/src/js/index.js
@@ -1,3 +1,21 @@
 // Global app controller
-import x from './test';
-console.log(x);
+import axios from 'axios';
+
+async function getResults(query) {
+  // const API_KEY = 'dcaa1bfe8797a1e7dfddf5adcbe72560';
+  const API_KEY = 'XXXdcaa1bfe8797a1e7dfddf5adcbe72560';
+  const PROXY = 'https://cors-anywhere.herokuapp.com/';
+  try {
+    const res = await axios(
+      `${PROXY}https://www.food2fork.com/api/search?key=${API_KEY}&q=${query}`,
+    );
+    console.log(res.data.recipes);
+  } catch (error) {
+    if (error.response) {
+      throw new Error(error.data);
+    }
+    throw new Error('Unauthorised');
+  }
+}
+
+getResults('ramen');
